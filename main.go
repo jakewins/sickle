1072d5269009856e046cc9a27f7b82ea2e0b6376
Jacob Hansson <jakewins@gmail.com>
2016-07-03 18:28:22 -0500

    Spike can now list available gazebo topics

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strconv"
 	"github.com/golang/protobuf/proto"
 	msg "github.com/jakewins/sickle/gazebo/v7.1/gazebo_msgs"
+	"errors"
 )
 
 func main() {
@@ -20,15 +21,57 @@ func main() {
 	}
 
 	reader := bufio.NewReader(conn)
-	for {
-		packet, nil := readGazeboPacket(reader)
-		if err != nil {
-			break // TODO
-		}
 
-		fmt.Println(packet.GetType())
-		break
+	init := readMessageOfType(reader, "version_init").(*msg.GzString)
+	if(init.GetData() != "gazebo 7.2") {
+		panic("Unknown Gazebo version: " + init.GetData())
 	}
+
+	namespaces := readMessageOfType(reader, "topic_namepaces_init").(*msg.GzString_V).GetData()
+	for _, namespace := range namespaces {
+		fmt.Println(namespace)
+	}
+
+	publishers := readMessageOfType(reader, "publishers_init").(*msg.Publishers)
+	for _, publisher := range publishers.GetPublisher() {
+		fmt.Println(publisher.GetHost() + ":" + strconv.Itoa(int(publisher.GetPort())) + "\t" + publisher.GetTopic())
+	}
+}
+
+func readMessageOfType(reader *bufio.Reader, assertType string) (message interface{}) {
+	gotType, message, err := readGazeboMessage(reader)
+	if err != nil {
+		panic(err)
+	}
+	if gotType != assertType {
+		panic(errors.New("Expected a '" + assertType + "' message, but got '"+gotType+"'"))
+	}
+	return message
+}
+
+func readGazeboMessage(reader *bufio.Reader) (msgtype string, message interface{}, err error) {
+	packet, err := readGazeboPacket(reader)
+	if err != nil {
+		return "", nil, err
+	}
+	msgtype = packet.GetType()
+
+	switch msgtype {
+	case "version_init":
+		message = &msg.GzString{}
+	case "topic_namepaces_init":
+		message = &msg.GzString_V{}
+	case "publishers_init":
+		message = &msg.Publishers{}
+	default:
+		return msgtype, nil, errors.New("Unknown message type: " + packet.GetType())
+	}
+	err = proto.Unmarshal(packet.GetSerializedData(), message.(proto.Message))
+	if err != nil {
+		return msgtype, nil, err
+	}
+
+	return msgtype, message, nil
 }
 
 func readGazeboPacket(reader *bufio.Reader) (packet *msg.Packet, err error) {
