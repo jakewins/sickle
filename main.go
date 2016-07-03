1b64ca9bd65d5fafedd21fe2d588ce9826a86547
Jacob Hansson <jakewins@gmail.com>
2016-07-03 15:05:46 -0500

    Very basic usage of protobuf, but it seems to work..

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 	"net"
 	"os"
 	"strconv"
-	"math"
-	"gazebo/v7.1/gazebo_msgs"
+	"github.com/golang/protobuf/proto"
+	msg "github.com/jakewins/sickle/gazebo/v7.1/gazebo_msgs"
 )
 
 func main() {
+
 	conn, err := net.Dial("tcp", "127.0.0.1:11345")
 	if err != nil {
 		fmt.Println(err)
@@ -21,37 +22,50 @@ func main() {
 	reader := bufio.NewReader(conn)
 	for {
 		// Header: 8 bytes, a hex string denoting the length of the packet
-		str, err := string(readChunk(reader, 8))
+		chunk, err := readChunk(reader, 8)
 		if err != nil {
 			break // TODO
 		}
-		packetLen, err := strconv.ParseInt(str, 16, 32)
+		packetLen, err := strconv.ParseInt(chunk.String(), 16, 32)
 		if err != nil {
 			break // TODO
 		}
-		packet, err := readChunk(packetLen)
+		chunk, err = readChunk(reader, int(packetLen))
+		if err != nil {
+			break // TODO
+		}
+		packet := &msg.Packet{}
+		err = proto.Unmarshal(chunk.Bytes(), packet)
 		if err != nil {
 			break // TODO
 		}
 
+		fmt.Println(packet.Type)
 		break
 	}
 }
 
-func readChunk(reader *bufio.Reader, len int) (bytes.Buffer, error) {
+func readChunk(reader *bufio.Reader, chunkSize int) (chunk bytes.Buffer, err error) {
 	var buf [1024]byte
 	var out bytes.Buffer
-	var remaining = len
+	var remaining = chunkSize
 
 	for remaining > 0 {
-		read, err := reader.Read(buf[:math.min(len(buf), remaining)])
+		read, err := reader.Read(buf[:min(len(buf), remaining)])
 		remaining -= read
 		if err != nil {
-			return nil, err
+			return out, err
 		}
 		if read > 0 {
 			out.Write(buf[:read])
 		}
 	}
 	return out, nil
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
