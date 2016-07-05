49ee0e9e4b30873671fa4bf1f9fe70f35503866f
Jacob Hansson <jakewins@gmail.com>
2016-07-04 22:04:42 -0500

    Skeleton spike for having Gazebo reverse-connect to us
    Gazebo uses a fascinating protocol, where it pretends sockets
    are simplex and unmuxxable, meaning every single stream we want
    to open has it's own socket.
    
    The way you send a message to Gazebo is:
    
    1) Send a message to Gazebo, telling it you want it to connect to
       a different socket, where you will proceed to send it a message
    2) Wait for Gazebo to connect to the second socket
    3) Send the message you actually wanted to send
    4) Close the socket
    
    I've gotten to (2) - but haven't been able to get Gazebo to reverse
    connect yet.

--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"strings"
 	"fmt"
 	"net"
-	"os"
 	"strconv"
 	"github.com/golang/protobuf/proto"
 	msg "github.com/jakewins/sickle/gazebo/v7.1/gazebo_msgs"
 	"errors"
+	"time"
 )
 
 func main() {
 
 	conn, err := net.Dial("tcp", "127.0.0.1:11345")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
 
 	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
 
 	init := readMessageOfType(reader, "version_init").(*msg.GzString)
 	if(init.GetData() != "gazebo 7.2") {
@@ -34,12 +35,91 @@ func main() {
 
 	publishers := readMessageOfType(reader, "publishers_init").(*msg.Publishers)
 	for _, publisher := range publishers.GetPublisher() {
-		fmt.Println(publisher.GetHost() + ":" + strconv.Itoa(int(publisher.GetPort())) + "\t" + publisher.GetTopic())
+		fmt.Println(publisher.GetHost() + ":" +
+			strconv.Itoa(int(publisher.GetPort())) +
+			"\t" + publisher.GetTopic() +
+			"\t" + publisher.GetMsgType())
+	}
+
+	server, err := net.Listen("tcp", "0.0.0.0:0")
+	if err != nil {
+		panic(err)
+	}
+
+	parts := strings.Split(server.Addr().String(), ":")
+	port, err := strconv.Atoi(parts[len(parts)-1])
+	if err != nil {
+		panic(err)
+	}
+
+	advert := &msg.Publish{
+		Topic: proto.String("/gazebo/default/Sickle::link_2_JOINT_4/joint_cmd"),
+		MsgType: proto.String("gazebo.msgs.JointCmd"),
+		Host: proto.String("127.0.0.1"),
+		Port: proto.Uint32(uint32(port)),
+	}
+
+	if err = writeMessage(writer, "advertise", advert); err != nil {
+		panic(err)
+	}
+	if err = writer.Flush(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Waiting for connection..")
+	reverseConn, err := server.Accept()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Got a caller!")
+	reverseReader := bufio.NewReader(reverseConn)
+	for {
+		msgtype, _, err := readMessage(reverseReader)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(msgtype)
 	}
 }
 
+func writeMessage(writer *bufio.Writer, msgtype string, message proto.Message) (err error) {
+	marshalled, err := proto.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	timestamp := time.Now()
+	marshalled, err = proto.Marshal(&msg.Packet{
+		Stamp: &msg.Time{
+			Sec: proto.Int32(int32(timestamp.Unix())),
+			Nsec: proto.Int32(int32(timestamp.Nanosecond())),
+		},
+		Type: proto.String(msgtype),
+		SerializedData:marshalled,
+	})
+	if err != nil {
+		return err
+	}
+
+	// TODO fmt should be able to do this with %08x
+	packetLen := lpad(strconv.FormatInt(int64(len(marshalled)), 16), "0", 8)
+	_, err = writer.Write([]byte(packetLen))
+	if err != nil {
+		// written == len(packetLen) | err == nil, so no looping needed
+		return err
+	}
+
+	_, err = writer.Write(marshalled)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func readMessageOfType(reader *bufio.Reader, assertType string) (message interface{}) {
-	gotType, message, err := readGazeboMessage(reader)
+	gotType, message, err := readMessage(reader)
 	if err != nil {
 		panic(err)
 	}
@@ -49,8 +129,8 @@ func readMessageOfType(reader *bufio.Reader, assertType string) (message interfa
 	return message
 }
 
-func readGazeboMessage(reader *bufio.Reader) (msgtype string, message interface{}, err error) {
-	packet, err := readGazeboPacket(reader)
+func readMessage(reader *bufio.Reader) (msgtype string, message interface{}, err error) {
+	packet, err := readPacket(reader)
 	if err != nil {
 		return "", nil, err
 	}
@@ -74,7 +154,7 @@ func readGazeboMessage(reader *bufio.Reader) (msgtype string, message interface{
 	return msgtype, message, nil
 }
 
-func readGazeboPacket(reader *bufio.Reader) (packet *msg.Packet, err error) {
+func readPacket(reader *bufio.Reader) (packet *msg.Packet, err error) {
 	// Header: 8 byte hex string denoting length of payload
 	chunk, err := readChunk(reader, 8)
 	if err != nil {
@@ -122,4 +202,10 @@ func min(x, y int) int {
 		return x
 	}
 	return y
+}
+
+func lpad(s string, padStr string, targetLen int) string {
+	padLen := 1 + ((targetLen - len(padStr)) / len(padStr))
+	result := strings.Repeat(padStr, padLen) + s
+	return result[(len(result) - targetLen):]
 }
